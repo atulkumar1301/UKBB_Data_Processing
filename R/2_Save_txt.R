434dbc3d09ced431a19b0c85cfcc007c3f836c7a
ATUL KUMAR <atul.0298@gmail.com>
2023-12-08 10:44:48 +0100

    Create 2_Save_txt.R

---
+++ R/2_Save_txt.R
@@ -0,0 +1,5 @@
+### This code keeps only the zeroth instance in the UKBB Data File
+library(data.table)
+options(scipen = 999) ## To avoid scientific notation
+df <- fread ("/Volumes/ATUL_6TB/Data/UKBB/Olink_Data/Olink_New/Olink_Data_Full.csv")
+write.table (df_1, "/Volumes/ATUL_6TB/Data/UKBB/Olink_Data/Olink_New/2_Olink_Data_T.txt")), sep="\t", quote=FALSE, row.names=FALSE, col.names=TRUE)
