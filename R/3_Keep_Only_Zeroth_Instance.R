f6d6f8ada51804121b4e1f9c441bf311a12b28d4
ATUL KUMAR <atul.0298@gmail.com>
2024-01-09 12:53:37 +0100

    Update 3_Keep_Only_Zeroth_Instance.R

--- R/3_Keep_Only_Zeroth_Instance.R
+++ R/3_Keep_Only_Zeroth_Instance.R
@@ -2,4 +2,5 @@
 library(data.table)
 options(scipen = 999) ## To avoid scientific notation
 df <- fread ("/Volumes/ATUL_6TB/Data/UKBB/Olink_Data/Olink_New/2_Olink_Data_T.txt")
+df_1 <- subset (df, df$ins_index == 0)
 write.table (df_1, "/Volumes/ATUL_6TB/Data/UKBB/Olink_Data/Olink_New/3_Olink_Data_Full_O_Instance.txt")), sep="\t", quote=FALSE, row.names=FALSE, col.names=TRUE)
